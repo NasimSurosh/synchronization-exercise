7c11804cc194df983e5d3e3b86199e0e0c69185c
NasimSursh <suroshzahiri@gmail.com>
2024-02-10 11:49:45 +0100

    synchronization exercise

---
+++ src/synchronizationBloc/Application1.java
@@ -0,0 +1,13 @@
+package synchronizationBloc;
+
+public class Application1 {
+
+  public static void main(String[] args) {
+    
+    Calculation1 calc = new Calculation1();
+    
+    calc.main();
+
+  }
+
+}
